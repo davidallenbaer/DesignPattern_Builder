2ddbf76b93ba82e7655b0f39219e900b773da172
davidallenbaer <davidallenbaer@gmail.com>
2021-05-16 18:53:26 -0400

    Add project files.

---
+++ DesignPattern_Builder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_Builder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Client Code
+            Report report;
+            ReportDirector reportDirector = new ReportDirector();
+            // Construct and display Reports
+            PDFReport pdfReport = new PDFReport();
+            report = reportDirector.MakeReport(pdfReport);
+            report.DisplayReport();
+            Console.WriteLine("-------------------");
+            ExcelReport excelReport = new ExcelReport();
+            report = reportDirector.MakeReport(excelReport);
+            report.DisplayReport();
+
+            Console.ReadKey();
+        }
+    }
+}
